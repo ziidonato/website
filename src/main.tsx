dd7822b3b7d995a399f3825759bff03e02588973
Zii Donato <zii@DESKTOP-GPJGI06>
2023-06-20 04:53:27 -0400

    changed yarn.lock

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import PageOverlay from './components/base/PageOverlay.tsx'
-import Header from './components/header/Header.tsx'
+import App from './App'
+import PageOverlay from './components/base/PageOverlay'
+import Header from './components/header/Header'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
